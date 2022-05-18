d4fae8f2302b0d69750ae0ef902e4930b0a2bf5b
Artifiction1 <angelus1@inbox.lv>
2022-05-18 10:23:01 +0300

    test.html

--- functions.js
+++ functions.js
@@ -1,6 +1,6 @@
 let correctAnswers = 0
 let wrongAnswers = 0
-let totalquestions = 0
+let totalquestions = 7
 let questions = [{question: "what is 2+2?", correctAnswer:"4", wrongAnswers:["3", "5", "6"]}, {question: "which of these is a prime number?", correctAnswer: 3 , wrongAnswers: [4, 9, 12]}, {question: "This is a question.", correctAnswer: "true", wrongAnswers:"false"},
 {question: "earth is the 7th planet in our solar system.", correctAnswer: "true", wrongAnswers:"false"}, {question: "which one was the 1st mars rover?", correctAnswer: "Mars Pathfinder", wrongAnswers: ["Opportunity", "curiosity", "Sojourner"]}, {question: "who wsa the 1st person in space?", correctAnswer: "Yuri Gagarin", wrongAnswers: ["Neil Armstrong", "Buzz Aldrin", "Matt Damon"]},
 {question: "how much of the land is covered in water?", correctAnswer:"0%", wrongAnswers:["71%", "68%", "80%"]}]
