9df883f28e574b858dd0efbc44905078bedc7c63
sweta sriramadasu <swethasriramdas2311@gmail.com>
2022-05-19 13:09:47 +0200

    refactoring of code on proceed button

--- welcome.js
+++ welcome.js
@@ -5,4 +5,28 @@ function check() {
   function uncheck() {
     document.getElementsByClassName("checkbox").checked = false;
   }
+  let cb = document.querySelector('.checkbox')
   
+  
+  function checked(){
+    console.log(cb.value)
+   let clickable = document.getElementById('link')
+   if (cb.checked == true){
+     clickable.href = "test.html"
+   }else{
+     clickable.href = ""
+   }
+ 
+}
+function proceed() {
+  let checkbox = document.getElementById("checkbox")
+  let proceed = document.getElementById("button")
+  proceed.addEventListener("click", function (e) {
+      if (checkbox.checked === false) {
+          alert("checkbox is checked to proceed!")
+      } else if (checkbox.checked === true) {
+          location.href = "questionPage.html"
+      }
+  })
+}
+
