3dd4179fd7f6ac8696b3eb6969ee5573a49a1cca
Naveen Kumar Manchala <naveenmanchala9698@gmail.com>
2022-05-20 09:37:25 +0200

    Feedback Page final

--- feedback.js
+++ feedback.js
@@ -1,20 +1,22 @@
-<<<<<<< HEAD
+
 function enableButton() {
     let textEnable = document.getElementById('comment').value
     let buttonEnable = document.getElementById('moreInfo')
+    let clickLink = document.getElementById('link')
     if(textEnable.length >= 10) {
         buttonEnable.removeAttribute('disabled')
+        clickLink.href = "https://epicode.com/it/"
     }else {
         buttonEnable.disabled = "true"
+        clickLink.href = ""
     }
 }
 
-=======
-function textButton(){
+/* function textButton(){
     textBox = document.getElementById("textBox")
     if(textBox.value === ""){
         }else{
             console.log(textBox.value)
         }
 }
->>>>>>> develop
+*/
